c17e261249632cb2974f107f29a0ae632e6efea9
So Cool <127.0.0.1.pm@gmail.com>
2023-09-05 17:36:32 +0700

    add initial state to memory history

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,8 +4,12 @@ import { createMemoryHistory, createBrowserHistory } from "history";
 import App from "./App";
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -16,7 +20,7 @@ const mount = (el, { onNavigate, defaultHistory }) => {
   return {
     onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
-      console.log("Container just navigated");
+      console.log("Container just navigated nextPathname:", nextPathname);
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
