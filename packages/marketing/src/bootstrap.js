48781d31f6635cbd3f84874271025976804f62a6
So Cool <127.0.0.1.pm@gmail.com>
2023-09-05 14:09:19 +0700

    sync browser history

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createMemoryHistory } from "history";
 import App from "./App";
 
 //mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+  history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
 };
 // if env is development and in isolation
 // call mount immediately
